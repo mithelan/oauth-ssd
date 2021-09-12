1a51a4f81b1c75cf01f3b9673e9b635a39c1a102
mithelan <mithelandev@gmail.com>
2021-09-12 21:02:19 +0530

    added oauth into env

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Container} from "@material-ui/core"
+import { GoogleLogin } from 'react-google-login';
+import { useState } from 'react';
+import env from "react-dotenv";
+
+import GooglePicker from "react-google-picker";
+
+const scope = [
+  "https://www.googleapis.com/auth/drive.readonly",
+  "https://www.googleapis.com/auth/drive.photos.readonly"
+];
+
 
 function App() {
+
+  const [profile,setProfile]=useState(false);
+
+// const responseGoogle = (response) => {
+//   console.log(response);
+//   console.log(response.profileObj);
+//   if(response.profileObj !== undefined){
+//     setProfile(true);
+//   }
+// }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      
+      {/* {!profile ?
+       <GoogleLogin
+    clientId={CLIENT_ID}
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+  : "Thala"} */}
+
+<GooglePicker
+        clientId={env.CLIENT_ID}
+        developerKey={env.API_KEY}
+        scope={scope}
+        // onAuthFailed={this.onAuthFail}
+        navHidden
+        mimeTypes={["image/png", "image/jpeg", "image/jpg"]}
+        createPicker={(google, oauthToken) => {
+          const picker = new google.picker.PickerBuilder()
+            .addView(new google.picker.View(google.picker.ViewId.DOCS_IMAGES))
+            .addView(new google.picker.DocsUploadView())
+            .setOAuthToken(oauthToken)
+            .setDeveloperKey(env.API_KEY)
+            .setAppId(env.APP_ID)
+            .setCallback((data) => {
+              if (data.action === google.picker.Action.PICKED) {
+                var fileId = data.docs[0].id;
+                alert("The user selected: " + fileId);
+              }
+            })
+            .enableFeature(google.picker.Feature.NAV_HIDDEN)
+            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED);
+
+          picker.build().setVisible(true);
+        }}
+      >
+        <button>
+         Continue with google
+        </button>
+      </GooglePicker>
+
+
+
+    </Container>
   );
 }
 
