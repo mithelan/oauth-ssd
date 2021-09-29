43917d8e1a498b6a25ed2da0b1a93a173067db2b
Manikavasagar Anjanan <it18186048@my.sliit.lk>
2021-09-29 10:21:04 +0530

    linked in login added

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'linkedinAuth': {
+    'clientID': '86ypd7wgjz2l68', // your App ID
+    'clientSecret': 'PKNT0yFZlMsqkFBp', // your App Secret
+    'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+  }
+}
